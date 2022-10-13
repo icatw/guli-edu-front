722769114c9d9b55501af3f9c3fc71d62c34402d
icatw <762188827@qq.com>
2022-10-13 20:58:04 +0800

    修改网关

--- template/utils/request.js
+++ template/utils/request.js
@@ -5,7 +5,7 @@ import { MessageBox, Message } from 'element-ui'
 import cookie from 'js-cookie'
 // 创建axios实例
 const service = axios.create({
-  baseURL: 'http://localhost:9001', // api的base_url,根据nginx端口号来写
+  baseURL: 'http://localhost:8222', // api的base_url,根据nginx端口号来写
   timeout: 20000 // 请求超时时间
 })
 // http request 拦截器
